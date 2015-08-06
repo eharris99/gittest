1128493ed465b7cdd433aa4f3a7c6e8f397f53ed
Elise Harris <eliseharris@Elises-MacBook-Air.local>
2015-08-06 19:35:09 -0400

    Initial Commit

---
+++ gittest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by Elise Harris on 8/6/15.
+//  Copyright (c) 2015 Elise Harris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
